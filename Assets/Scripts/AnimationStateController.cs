8d329f846ada6d0abafd6b694fc198988f2fae18
Thanh To Chi <thanh.to-chi@ubisoft.com>
2021-12-29 14:09:51 +0700

    finish https://www.youtube.com/watch?v=m8rGyoStfgQ&list=PLwyUzJb_FNeTQwyGujWRLqnfKpV-cj-eO&index=5

--- Assets/Scripts/AnimationStateController.cs
+++ Assets/Scripts/AnimationStateController.cs
@@ -5,32 +5,49 @@ using UnityEngine;
 public class AnimationStateController : MonoBehaviour
 {
     private Animator _animator;
-    private int _isWalkingHash;
-    private int _isRunningHash;
+    //private int _isWalkingHash;
+    //private int _isRunningHash;
+    private float _velocity = 0.0f;
+    private int _velocityHash;
+    private float _acceleration = 0.1f;
+    private float _deceleration = 0.5f;
 
     // Start is called before the first frame update
     void Start()
     {
         _animator = GetComponent<Animator>();
-        _isWalkingHash = Animator.StringToHash("isWalking");
-        _isRunningHash = Animator.StringToHash("isRunning");
+        //_isWalkingHash = Animator.StringToHash("isWalking");
+        //_isRunningHash = Animator.StringToHash("isRunning");
+        _velocityHash = Animator.StringToHash("velocity");
     }
 
     // Update is called once per frame
     void Update()
     {
-        bool isWalking = _animator.GetBool(_isWalkingHash);
-        bool isRunning = _animator.GetBool(_isRunningHash);
+        //bool isWalking = _animator.GetBool(_isWalkingHash);
+        //bool isRunning = _animator.GetBool(_isRunningHash);
+        //bool forwardPressed = Input.GetKey("w");
+        //bool runPressed = Input.GetKey("left shift") && forwardPressed;
+        //if (isWalking != forwardPressed)
+        //{
+        //    _animator.SetBool(_isWalkingHash, forwardPressed);
+        //}
+        //if (isRunning != runPressed)
+        //{
+        //    _animator.SetBool(_isRunningHash, runPressed);
+        //}    
+
         bool forwardPressed = Input.GetKey("w");
-        bool runPressed = Input.GetKey("left shift") && forwardPressed;
-        if (isWalking != forwardPressed)
+        bool runPressed = Input.GetKey("left shift");
+        if (forwardPressed)
         {
-            _animator.SetBool(_isWalkingHash, forwardPressed);
+            _velocity += Time.deltaTime * _acceleration;
         }
-        if (isRunning != runPressed)
+        else
         {
-            _animator.SetBool(_isRunningHash, runPressed);
-        }    
-        
+            _velocity -= Time.deltaTime * _deceleration;
+        }
+        _velocity = Mathf.Clamp(_velocity, 0, 1);
+        _animator.SetFloat(_velocityHash, _velocity);
     }
 }
